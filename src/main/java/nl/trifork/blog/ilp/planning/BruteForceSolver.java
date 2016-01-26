4339be27f96227936a515919ef6efc5864e686a6
Marijn van Zelst <marijn.vanzelst@trifork.nl>
2016-01-26 10:00:24 +0100

    Added two solvers, added libraries

---
+++ src/main/java/nl/trifork/blog/ilp/planning/BruteForceSolver.java
@@ -0,0 +1,76 @@
+package nl.trifork.blog.ilp.planning;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class BruteForceSolver {
+
+    public static void main(String args[]){
+        // List of pois
+        List<String> pois = IntStream.range(0, 1000)
+                .mapToObj(i -> "poi" + i)
+                .collect(Collectors.toList());
+
+        // Weight per poi
+        Map<String, Integer> weightPerPoi = new HashMap<>();
+        Random random = new Random();
+        for (String poi : pois) {
+            weightPerPoi.put(poi, random.nextInt(10000) + 1);
+        }
+
+
+        BigInteger loopCounter = new BigInteger("0");
+        BigInteger totalLoops = new BigInteger("1000000000000000000000");
+        BigInteger showProgressInterval = new BigInteger("10000000");
+        long start = System.currentTimeMillis();
+        int maxSum = 0;
+        for (String poiTimeslot1 : pois) {
+            for (String poiTimeslot2 : pois) {
+                for (String poiTimeslot3 : pois) {
+                    for (String poiTimeslot4 : pois) {
+                        for (String poiTimeslot5 : pois) {
+                            for (String poiTimeslot6 : pois) {
+                                // Calculate the sum of recommendations
+                                int sum = weightPerPoi.get(poiTimeslot1) +
+                                        weightPerPoi.get(poiTimeslot2) +
+                                        weightPerPoi.get(poiTimeslot3) +
+                                        weightPerPoi.get(poiTimeslot4) +
+                                        weightPerPoi.get(poiTimeslot5) +
+                                        weightPerPoi.get(poiTimeslot6);
+
+                                // Check if we have a new best solution
+                                if(sum > maxSum){
+                                    maxSum = sum;
+                                    List<String> solution = Arrays.asList(poiTimeslot1,
+                                            poiTimeslot2,
+                                            poiTimeslot3,
+                                            poiTimeslot4,
+                                            poiTimeslot5,
+                                            poiTimeslot6);
+                                    System.out.println("Found new best solution: " + solution + ", sum:" + sum);
+                                }
+
+                                // Update the progress
+                                loopCounter = loopCounter.add(BigInteger.ONE);
+
+                                // Print estimation of total run time
+                                if(loopCounter.mod(showProgressInterval).equals(BigInteger.ZERO)){
+                                    BigDecimal ratioComplete = new BigDecimal(loopCounter).divide(new BigDecimal(totalLoops));
+                                    BigDecimal multiplierTodo = BigDecimal.ONE.divide(ratioComplete, MathContext.DECIMAL128);
+                                    BigDecimal totalDurationInMilliseconds = multiplierTodo.multiply(new BigDecimal(System.currentTimeMillis() - start));
+                                    BigInteger durationInYears = totalDurationInMilliseconds.divide(new BigDecimal(1000 * 60 * 60 * 24 * 365), MathContext.DECIMAL128).toBigInteger();
+                                    System.out.println(String.format("Total duration in years: %s", durationInYears));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ src/main/java/nl/trifork/blog/ilp/planning/IlpSolver.java
@@ -0,0 +1,129 @@
+package nl.trifork.blog.ilp.planning;
+
+import com.google.ortools.linearsolver.MPConstraint;
+import com.google.ortools.linearsolver.MPObjective;
+import com.google.ortools.linearsolver.MPSolver;
+import com.google.ortools.linearsolver.MPVariable;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class IlpSolver {
+
+    public void solve() {
+        List<String> pois = IntStream.range(0, 1000)
+                .mapToObj(i -> "poi" + i)
+                .collect(Collectors.toList());
+
+        List<String> timeslots = IntStream.range(0, 6)
+                .mapToObj(i -> "timeslot" + i)
+                .collect(Collectors.toList());
+
+
+        MPSolver solver = createSolver();
+        List<MPVariable> allVariables = new ArrayList<>();
+        Map<String, List<MPVariable>> variablesGroupedByPoi = new HashMap<>();
+        Map<String, List<MPVariable>> variablesGroupedByTimeslot = new HashMap<>();
+        for (String timeslot : timeslots) {
+            for (String poi : pois) {
+                MPVariable mpVariable = solver.makeIntVar(0.0, MPSolver.infinity(), timeslot + "_" + poi);
+
+                allVariables.add(mpVariable);
+
+                // Keep track of the variables per poi
+                if (!variablesGroupedByPoi.containsKey(poi)) {
+                    variablesGroupedByPoi.put(poi, new ArrayList<>());
+                }
+                variablesGroupedByPoi.get(poi).add(mpVariable);
+
+                // Keep track of the variables per timeslot
+                if (!variablesGroupedByTimeslot.containsKey(timeslot)) {
+                    variablesGroupedByTimeslot.put(timeslot, new ArrayList<>());
+                }
+                variablesGroupedByTimeslot.get(timeslot).add(mpVariable);
+            }
+        }
+
+        // Create objective function
+        MPObjective objective = solver.objective();
+        Random random = new Random();
+        for (MPVariable mpVariable : allVariables) {
+            int randomRecommendationScore = random.nextInt(10000) + 1;
+            // Assign a random recommendation score to the variable
+            objective.setCoefficient(mpVariable, randomRecommendationScore);
+        }
+        // Maximize the outcome of the object function
+        objective.setMaximization();
+
+        // Set the constraints to visit poi at most once in all timeslots
+        for (Map.Entry<String, List<MPVariable>> mpVariablesPerPoi : variablesGroupedByPoi.entrySet()) {
+            double constraintMinimumValue = 0D;
+            double constraintMaximumValue = 1D;
+            String constraintDescription = "visit poi " + mpVariablesPerPoi.getKey() + " at most once";
+            MPConstraint constraint = solver.makeConstraint(
+                    constraintMinimumValue,
+                    constraintMaximumValue,
+                    constraintDescription);
+
+            for (MPVariable mpVariable : mpVariablesPerPoi.getValue()) {
+                double multiplierForVariable = 1D;
+                constraint.setCoefficient(mpVariable, multiplierForVariable);
+            }
+        }
+
+        // Set the constraints to use timeslot at most once for all pois
+        for (Map.Entry<String, List<MPVariable>> mpVariablesPerTimeslot : variablesGroupedByTimeslot.entrySet()) {
+            double constraintMinimumValue = 0D;
+            double constraintMaximumValue = 1D;
+            String constraintDescription = "use timeslot " + mpVariablesPerTimeslot.getKey() + " at most once";
+            MPConstraint constraint = solver.makeConstraint(
+                    constraintMinimumValue,
+                    constraintMaximumValue,
+                    constraintDescription);
+
+            for (MPVariable mpVariable : mpVariablesPerTimeslot.getValue()) {
+                double multiplierForVariable = 1D;
+                constraint.setCoefficient(mpVariable, multiplierForVariable);
+            }
+        }
+
+        MPSolver.ResultStatus resultStatus = solver.solve();
+
+        // Check that the problem has an optimal solution.
+        if (resultStatus != MPSolver.ResultStatus.OPTIMAL) {
+            System.err.println("The problem does not have an optimal solution!");
+            return;
+        }
+
+        // Print solution
+        System.out.println("Problem solved in " + solver.wallTime() + " milliseconds");
+        System.out.println("Optimal objective value = " + solver.objective().value());
+        for (MPVariable mpVariable : allVariables) {
+            if (mpVariable.solutionValue() > 0) {
+                System.out.println("Visit: " + mpVariable.name() + ", weight: " + objective.getCoefficient(mpVariable));
+            }
+        }
+    }
+
+    private MPSolver createSolver() {
+        try {
+            return new MPSolver("LinearProgrammingExample", MPSolver.OptimizationProblemType.GLOP_LINEAR_PROGRAMMING);
+        } catch (IllegalArgumentException e) {
+            return null;
+        }
+    }
+
+    public static void main(String args[]){
+        try {
+            System.load(new ClassPathResource("lib/linux64/libjniortools.so").getFile().getAbsolutePath());
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load or tools library");
+        }
+
+        IlpSolver ilpSolver = new IlpSolver();
+        ilpSolver.solve();
+    }
+}
